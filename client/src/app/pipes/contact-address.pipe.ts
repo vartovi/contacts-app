9b5e61a6b9067e989752227e175dc56170d44562
vartovi <viljo.varto@gmail.com>
2017-04-18 10:15:25 +0300

    Included missing file

---
+++ client/src/app/pipes/contact-address.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Contact } from "../contact-list/contact";
+import * as _ from 'lodash';
+
+@Pipe({
+  name: 'contactAddress'
+})
+export class ContactAddressPipe implements PipeTransform {
+
+  transform(contact: Contact, args?: any): any {
+
+    let addressParts = [contact.streetAddress || null, contact.city || null];
+    addressParts =  _.reject(addressParts, _.isNull);
+
+    return addressParts.join(', ');
+  }
+
+}
